276da8a7d8433f552b1a15e47a7956bfafc146d0
eunyoung123 <dms1009dud@gmail.com>
2022-09-15 09:27:30 +0900

    test2

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // git_test test
+        // test2
     }
 }
