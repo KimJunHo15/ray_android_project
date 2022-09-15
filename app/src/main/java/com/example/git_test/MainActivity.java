d090f97ccb60c3f128d20672db111cf9e2ac6a38
eunyoung123 <dms1009dud@gmail.com>
2022-09-15 11:35:00 +0900

    main 화면 구성

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.git_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img_exam,img_game,img_info,img_my;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // git_test test
         // test2
+        img_exam = findViewById(R.id.img_exam);
+        img_game = findViewById(R.id.img_game);
+        img_info = findViewById(R.id.img_info);
+        img_my = findViewById(R.id.img_my);
+
+
+
     }
 }
