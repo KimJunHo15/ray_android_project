6ce4f253cf363707bb6f9593aac0f60da321483b
eunyoung123 <dms1009dud@gmail.com>
2022-09-29 18:46:42 +0900

    치매정보 메인 뒤로가기 버튼 수정함

--- app/src/main/java/com/example/git_test/MainFragment.java
+++ app/src/main/java/com/example/git_test/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.git_test;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class MainFragment extends Fragment {
@@ -15,6 +17,16 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ImageView img_return_main_f = view.findViewById(R.id.img_return_main_f);
+
+        img_return_main_f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext().getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
